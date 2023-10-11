640d8a159a41d7aa145b793d79efe2e2b4d1c722
Jenier Suarez <51455404+jjaviersuarez@users.noreply.github.com>
2023-10-11 09:41:14 -0500

    Aplicación con mensaje de bienvenida

--- cursos/views.py
+++ cursos/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#Página Index de Nuestra Aplicación
+def index(request):
+    return render(request,'index.html')
+
 def inicio(request):
     return render(request,'inicio.html')
+
--- mi_clase/urls.py
+++ mi_clase/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from cursos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
     path('cursos/',include('cursos.urls'))
 ]
